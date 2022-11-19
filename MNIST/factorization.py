df290a40616ac8460f8c94bdb7b17ce79ef8b1cc
Matheus Farias <matheusfarias@g.harvard.edu>
2022-11-19 12:23:23 -0500

    --amend

--- MNIST/factorization.py
+++ MNIST/factorization.py
@@ -19,7 +19,7 @@ print(torch.matmul(B, torch.linalg.pinv(B_prime)).shape)
 error = []
 for i in range(epochs):
     B_pos = torch.matmul(B, torch.linalg.pinv(B_prime))
-    B_pos = torch.abs(B_pos)
+    #B_pos = torch.abs(B_pos)
     B = torch.matmul(B_pos, B_prime)
     B_prime,_,_,_ = torch.linalg.lstsq(B_pos, B)
     error.append(torch.norm(B-B0))
